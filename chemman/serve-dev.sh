356d7f70ea3b5883419cc1aae95ca6db557d9609
Whitie <weimann.th@gmail.com>
2025-07-18 11:57:30 +0200

    Updated translations.

--- chemman/serve-dev.sh
+++ chemman/serve-dev.sh
@@ -18,6 +18,6 @@ $GUNI \
     --reload \
     --workers 4 \
     --access-logfile - \
-    --bind 127.0.0.1:8000 \
+    --bind 0.0.0.0:8000 \
     --env DJANGO_SETTINGS_MODULE=chemman.settings \
     chemman.wsgi:application
